363cf3721b6ced8a21a0b56d286ffcb433938072
Julian Kappler <jkappler@posteo.de>
2023-10-04 10:54:18 +0200

    added sorting algorithms and comparison of them

---
+++ helper_functions.py
@@ -0,0 +1,203 @@
+#!/usr/bin/env python
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+import copy
+rng = np.random.default_rng()
+
+plt.rcParams.update({
+    "text.usetex": True,
+    "font.family": "serif",
+    'font.size': 18,
+    "font.sans-serif": "computer modern",
+})
+
+
+def compare_algorithms(algorithms,
+                       Ns,
+                       N_samples=1000,
+                       sorted_input=False,
+                       ):
+
+    T = np.zeros([3,len(Ns),N_samples],dtype=int)
+    Times = np.zeros([3,len(Ns)],dtype=float)
+
+    for i,N in enumerate(Ns):
+        #
+        random_arrays = rng.integers(low=0,high=2*N,
+                                    size=N*N_samples)
+        random_arrays = random_arrays.reshape([N_samples,N])
+        #
+        for j,array in enumerate(random_arrays):
+            #
+            if j % 10 == 0:
+                print('N = {0}, sample # = {1}'.format(N,j+1),
+                    end='\r')
+            #
+            sorted_ref = np.sort(array)
+            if sorted_input:
+                array = copy.deepcopy(sorted_ref)
+            #print(N,sorted_ref)
+            #
+            c = 0
+            for name,algo in algorithms.items():
+                input_array = copy.deepcopy(array)
+                start = time.time()
+                result = algo.sort(input_array)
+                Times[c,i] += time.time()-start
+                if (sorted_ref != result['x']).any():
+                    #print(sorted_ref)
+                    #print(result['x'])
+                    raise RuntimeError("Algorithm {0}"\
+                        " returned unsorted array".format(name))
+                #
+                T[c,i,j] = result['T']
+                c += 1
+    #
+    return T, Times
+
+
+def plot_results(algorithms,
+                 Ns,
+                 T,
+                 Times,
+                 scalebars={0:'2',2:'log'}, 
+                 filename_prefix=None):
+        N_samples = len(T[0,0])
+        #
+        # First plot: 
+        # Mean number of array value comparisons per sorting
+        #
+        fig, ax = plt.subplots(1,1,figsize=(6,4))
+        fig.suptitle('Mean number of value comparisons per sorting')
+
+        for i,name in enumerate(algorithms.keys()):
+                ax.plot(Ns,np.mean(T[i],axis=-1),
+                        marker='o',
+                        label=name)
+        
+        #
+        ax.legend(loc='best',
+                fontsize=15,
+                )
+        ax.set_xlabel(r'$N$')
+        ax.set_ylabel(r'$\langle T \rangle$')
+        ax.set_xscale('log')
+        ax.set_yscale('log')
+        ylim = ax.get_ylim()
+        # scale bars 
+        xP = np.logspace(1.7,3,num=101)
+        indices = []
+        labels = []
+        yPs = []
+        for algo_index, functional_form in scalebars.items():
+                #
+                indices.append(algo_index)
+                #
+                if functional_form == '2':
+                        labels.append(r'$\sim N^2$')
+                        yPs.append( (xP/xP[0])**2 )
+                        factor = 15
+                        rotation=30
+                elif functional_form == 'log':
+                        labels.append(r'$\sim N \log (N)$')
+                        yPs.append( (xP/xP[0])*(np.log(xP)/np.log(xP[0])) )
+                        factor = 5
+                        rotation=20
+                else:
+                        raise RuntimeError("Functional form {0}"
+                        " not implemented".format(functional_form))
+                #
+                #
+                x_index = np.argmin(np.fabs(Ns - xP[0]))
+                v = np.mean(T[algo_index,x_index])
+                y_shifted = v * yPs[-1] * 1.3
+                ax.plot(xP,y_shifted,
+                            color='black',
+                            ls='--',
+                            )
+                x_text = 2e2
+                x_index = np.argmin(np.fabs(Ns - x_text))
+                v = y_shifted[x_index]
+                ax.text(x=x_text,
+                        y=factor*v,
+                        rotation=rotation,
+                        fontsize=18,
+                        s=labels[-1])
+        plt.show()
+        if filename_prefix is not None:
+                fig.savefig(filename_prefix + '_comparisons.pdf',
+                            bbox_inches='tight')
+                fig.savefig(filename_prefix + '_comparisons.png',
+                            bbox_inches='tight')
+        plt.close(fig)
+        #
+        #
+        # Second plot:
+        # Mean physical time elapsed per sorting
+        #
+        fig, ax = plt.subplots(1,1,figsize=(6,4))
+        fig.suptitle('Mean physical time elapsed per sorting')
+        ys = []
+        for i,name in enumerate(algorithms.keys()):
+                y = Times[i]/N_samples * 1e3
+                ax.plot(Ns,y,
+                        marker='o',
+                        label=name)
+                ys.append(y)
+        ax.set_xscale('log')
+        ax.set_yscale('log')
+        ax.set_xlabel(r'$N$')
+        ax.set_ylabel(r'$\langle \tau \rangle$ $($ms$)$')
+        # scale bars 
+        xP = np.logspace(2,3,num=101)
+        indices = []
+        labels = []
+        yPs = []
+        for algo_index, functional_form in scalebars.items():
+                #
+                indices.append(algo_index)
+                #
+                if functional_form == '2':
+                        labels.append(r'$\sim N^2$')
+                        yPs.append( (xP/xP[0])**2 )
+                        factor = 1.5
+                        rotation=30
+                elif functional_form == 'log':
+                        labels.append(r'$\sim N \log (N)$')
+                        yPs.append( (xP/xP[0])*(np.log(xP)/np.log(xP[0])) )
+                        factor = 1.5
+                        rotation=20
+                else:
+                        raise RuntimeError("Functional form {0}"
+                        " not implemented".format(functional_form))
+                #
+                #
+                x_index = np.argmin(np.fabs(Ns - xP[0]))
+                v = ys[algo_index][x_index]
+                y_shifted = 2*v * yPs[-1]
+                ax.plot(xP,y_shifted,
+                            color='black',
+                            ls='--',
+                            )
+                x_text = 2e2
+                x_index = np.argmin(np.fabs(xP - x_text))
+                v = y_shifted[x_index]
+                ax.text(x=x_text,
+                        y=v*factor,
+                        rotation=rotation,
+                        fontsize=18,
+                        s=labels[-1])
+
+        ax.legend(loc='best',
+                fontsize=15,
+                )
+        plt.show()
+        if filename_prefix is not None:
+                fig.savefig(filename_prefix + '_time.pdf',
+                            bbox_inches='tight')
+                fig.savefig(filename_prefix + '_time.png',
+                            bbox_inches='tight')
+        plt.close(fig)
---
+++ sorting_algorithms.py
@@ -0,0 +1,210 @@
+#!/usr/bin/env python
+
+
+
+
+class bubblesort:
+    '''
+    Bubblesort sorting algorithm
+    '''
+
+    def sort(self,x):
+        '''
+        Input:
+            list/np.array x: array to be ordered
+        Output:
+            dict output: contains keys
+                - 'x': sorted list/array
+                - 'T': number of comparisons made during sorting
+        '''
+        T = 0 # nummber of comparisons made during sorting
+        #
+        n = len(x)
+        swapped = True
+        #
+        while swapped:
+            #
+            swapped = False
+            #
+            for i in range(n-1):
+                #
+                T += 1
+                if x[i] > x[i+1]:
+                    x[i], x[i+1] = x[i+1], x[i]
+                    swapped = True
+            #
+            n -= 1
+        #
+        output = {'x':x,
+                  'T':T}
+        return output
+    
+class quicksort:
+    '''
+    Quicksort sorting algorithm with the following schemes
+    for selecting a pivot element:
+    - Lomuto scheme (last element of subarray is pivot),
+    - Hoare scheme (middle element of subarray is pivot).
+    The default mode is the Hoare scheme.
+
+    To select a scheme, either initialize the class with
+    mode = 'lomuto' or 'hoare', or call the function 
+    quicksort.sort of an initialized class with
+    mode = 'lomuto' or 'hoare'.
+
+    Example:
+    x = [3,1,2,5,2]
+
+    Q = quicksort()
+    result = Q.sort(x=x,
+                    mode='lomuto')
+    
+    print('sorted: x =',result['x'])
+
+    This prints:
+    sorted: x = [1, 2, 2, 3, 5]
+
+    WARNING: calling quicksort.sort with the mode argument provided
+    only specifies this mode for that particular sorting. If for 
+    subsequent no mode argument is provided, then the default mode
+    will be used (either 'hoare' of no mode has been set at initialization,
+    or the mode set at initialization).
+    For example, if you run
+    Q = quicksort(mode='lomuto')
+    result_x = Q.sort(x,mode='hoare')
+    result_y = Q.sort(y),
+    then y will be sorted using the lomuto scheme.
+
+    '''
+
+    def __init__(self,mode=None):
+        #
+        if mode is None:
+            self.mode = 'hoare'
+        else:
+            self.mode = mode
+
+
+    def partition_lomuto(self,
+                  l, # left
+                  r, # right
+                  ):
+        '''
+        This function partitions the subarray self.x[l:r+1] 
+        such that 
+        - all elements smaller than self.x[r] are 
+          moved to the left of the subarray, and 
+        - all elements larger than self.x[r] are 
+          moved to the right of the subarray.
+
+        arguments:
+            integers l, r: bounds of the subarray to be considered
+
+        returns:
+            integer i: the index of the pivot element
+        '''
+        #
+        p = self.x[r]
+        #
+        i = l
+        for j in range(l,r):
+            #
+            self.T += 1
+            if self.x[j] <= p:
+                #
+                self.x[i], self.x[j] = self.x[j], self.x[i]       
+                i += 1
+        #
+        self.x[i], self.x[r] = self.x[r], self.x[i]
+        #
+        return i
+
+    def partition_hoare(self,l,r):
+        #
+        p = self.x[ (l+r)//2 ]
+        #
+        i = l
+        j = r
+        #
+        while True:
+            #
+            self.T += 1
+            while (self.x[i] < p):
+                self.T += 1
+                i += 1
+            #
+            self.T += 1
+            while (self.x[j] > p):
+                self.T += 1
+                j -= 1
+            #
+            if i >= j:
+                return j
+            #
+            self.x[i], self.x[j] = self.x[j], self.x[i]
+            #
+            i += 1
+            j -= 1
+        #
+    
+    def qs(self,l,r):
+        '''
+        Main function of quicksort. Partitions a subarray and
+        then calls itself for each of the partitions
+
+        arguments:
+            integers l, r: bounds of the subarray to be considered
+        '''
+        #
+        if l >= r: # exit condition
+            return
+        #
+        p = self.partition(l=l,
+                           r=r)
+        #
+        self.qs(l=l,
+                r=p+self.dr)
+        self.qs(l=p+1,
+                r=r)
+
+
+    def sort(self,x,
+             mode=None):
+        '''
+        Call this function for starting the quicksort algorithm
+
+        arguments:
+            list/np.array x: array to be sorted
+
+        returns:
+            output: dictionary containing keys
+                - 'x': sorted array
+                - 'T': number of comparisons made during sorting
+        '''
+        #
+        # set mode
+        if mode is None:
+            mode = self.mode
+        #
+        if mode == 'lomuto':
+            self.dr = -1
+            self.partition = self.partition_lomuto
+        else:
+            self.dr = 0
+            self.partition = self.partition_hoare
+        #
+        # initialize variables
+        self.T = 0 # number of comparisons made during sorting
+        self.x = x
+        n = len(x)
+        #
+        # sort
+        self.qs(l=0,r=n-1)
+        #
+        # return result
+        output = {'x':self.x,
+                  'T':self.T}
+        return output
+
+   
+
